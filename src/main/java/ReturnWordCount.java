a7a81f085c07975e67c97befdbe843c61b4b528b
ritualFire <jpelaez456@gmail.com>
2023-03-08 03:31:45 +0000

    ReturnWordCount

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,14 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        int spaceCount = 0;
+
+        for(int i=0;i<in.length();i++){
+            if(in.charAt(i) == ' '){
+                spaceCount++;
+            }
+
+        }
+        return spaceCount+1;
     }
 }
